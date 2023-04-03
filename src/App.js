f8bc50a3a44833a3909312be54210d7ba9519454
nishita84 <91163759+nishita84@users.noreply.github.com>
2023-04-03 17:32:32 -0400

    Basic App Bar added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./Components/Button";
+import ButtonAppBar from "./Components/AppBar";
+
+function App() {
+  return (
+    <>
+      <div className="App"></div>
+      <ButtonAppBar />
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,3 @@
+export default function Button() {
+  return <button>this is a button!!</button>;
+}
