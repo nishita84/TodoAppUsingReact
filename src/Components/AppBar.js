910ca19fe1e08a0620b2ef9cc782743eebb72a43
nishita84 <91163759+nishita84@users.noreply.github.com>
2023-04-10 17:40:42 -0400

    Added logic for showing whatever is in the input

--- src/Components/AppBar.js
+++ src/Components/AppBar.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-//import AppBar from "@mui/material/AppBar";
 import { AppBar } from "@mui/material";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,14 +4,24 @@ import "../Style/style.css";
 import TextField from "@mui/material/TextField";
 
 export default function BasicCard() {
-  const [todo, setTodo] = React.useState(" ");
-  const [count, setCount] = React.useState(0);
+  const [input, setInput] = React.useState(null);
+  const [show, setShow] = React.useState(false);
 
-  console.log(count);
+  const getInput = (prop) => {
+    setInput(prop.target.value);
+    setShow(false);
+  };
+
   return (
     <Card className="decoratedcard">
-      <TextField id="standard-basic" label="Standard" variant="standard" />
-      <span>Button was clicked {count} times </span>
+      <TextField
+        onChange={getInput}
+        id="standard-basic"
+        label="Standard"
+        variant="standard"
+      ></TextField>
+      {show ? <span>{input}</span> : null}
+      <button onClick={() => setShow(true)}> Add to todo list </button>
     </Card>
   );
 }
